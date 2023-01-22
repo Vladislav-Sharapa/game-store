e772a676845e43220f5fe1c61e6c46d7fe96d841
Влад Шарапа <vl.sharapa20@gmail.com>
2023-01-22 20:06:20 +0300

    Create and register model

--- gamestore/shop/admin.py
+++ gamestore/shop/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Category, Game
 
 # Register your models here.
+
+
+class GameAdmin(admin.ModelAdmin):
+    # отображаемые поля в админке
+    list_display = ('id', 'title','price')
+    list_display_links = ('id', 'title')
+    # поиск по полям
+    search_fields = ('title', 'price')
+    prepopulated_fields = {'slug':('title',)}
+
+class CategoryAdmin(admin.ModelAdmin):
+    # отображаемые поля в админке
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    # поиск по полям
+    search_fields = ('name',)
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Game, GameAdmin)
+admin.site.register(Category)
--- gamestore/shop/models.py
+++ gamestore/shop/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.core.validators import MinValueValidator 
+from decimal import Decimal
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True,verbose_name="URL")
+    description = models.TextField(blank=True)
+    photo =models.ImageField(upload_to="photos/%Y/%m/%d/")
+    price = models.DecimalField(decimal_places=2, max_digits=7, validators=[MinValueValidator(Decimal('1.00'))])
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('games', kwargs={'id': self.id})
+    
+    class Meta:
+        verbose_name = "Игра"
+        verbose_name_plural = "Игры"
+        # порядок сортировки
+        ordering = ['title']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        # порядок сортировки
+        ordering = ['name']
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'id': self.id})
+
