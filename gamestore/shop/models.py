a66576ad96e9dc028793779911a224945c96da0a
Влад Шарапа <vl.sharapa20@gmail.com>
2023-01-24 17:50:11 +0300

    Update model

--- gamestore/shop/models.py
+++ gamestore/shop/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MinValueValidator 
 from decimal import Decimal
 from django.urls import reverse
-
+from PIL import Image
 # Create your models here.
 
 
@@ -10,7 +10,8 @@ class Game(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True, db_index=True,verbose_name="URL")
     description = models.TextField(blank=True)
-    photo =models.ImageField(upload_to="photos/%Y/%m/%d/")
+    vertical_photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    horizontal_photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
     price = models.DecimalField(decimal_places=2, max_digits=7, validators=[MinValueValidator(Decimal('1.00'))])
     category = models.ForeignKey('Category', on_delete=models.PROTECT)
 
@@ -18,8 +19,25 @@ class Game(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('games', kwargs={'id': self.id})
-    
+        return reverse('games', kwargs={'game_slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        vertical_img = Image.open(self.vertical_photo.path)
+        horizontal_img = Image.open(self.horizontal_photo.path)
+
+        if vertical_img.width != 400 or vertical_img.height != 500:
+            size = (400, 500)
+            vertical_img.thumbnail(size)
+            vertical_img.save(self.vertical_photo.path)
+
+        if horizontal_img.width != 450 or horizontal_img.height != 300:
+            size = (450, 300)
+            horizontal_img.thumbnail(size)
+            horizontal_img.save(self.horizontal_photo.path)
+
+
     class Meta:
         verbose_name = "Игра"
         verbose_name_plural = "Игры"
@@ -38,8 +56,8 @@ class Category(models.Model):
         ordering = ['name']
 
     def __str__(self):
-        return self.title
+        return self.name
 
     def get_absolute_url(self):
-        return reverse('category', kwargs={'id': self.id})
+        return reverse('category', kwargs={'category_slug': self.slug})
 
