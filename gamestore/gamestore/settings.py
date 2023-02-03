5deda100c9f1ffb5d3d481d09848466847feea91
Влад Шарапа <vl.sharapa20@gmail.com>
2023-02-03 14:09:01 +0300

    Create new app Cart

--- gamestore/gamestore/settings.py
+++ gamestore/gamestore/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
     'bootstrap5',
 ]
 
@@ -117,3 +118,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+CART_SESSION_ID = 'cart'
